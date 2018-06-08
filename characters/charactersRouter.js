59e75a8cb51d76d76c3622971c17731b27caac16
Ravani <Bhavik.Ravani@coxautoinc.com>
2018-06-08 13:49:17 -0400

    Updated /api/characters/:id and added /api/characters/:id/vehicles

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const Character = require('./Character.js');
 const Film = require('../films/Film.js');
-
+const Vehicle = require('../vehicles/Vehicle.js');
 const router = express.Router();
 
 // add endpoints here
-
 
 router.get("/", (req, res) => {
     Character.find()
@@ -15,14 +14,29 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
     let { id } = req.params;
-    const charByID = Character.findById(id)
-        .populate('homeworld')
+    const charById = Character.findById(id)
         .select('name gender skin_color hair_color height eye_color birth_year')
+        .populate('homeworld')
     const movies = Film.where({ characters: id })
         .select('title producer director episode release_date')
-    Promise.all([charByID, movies])
-        .then(response => res.status(200).json(response))
+    Promise.all([charById, movies])
+        .then(response => {
+            res.status(200).json({ ...response[0]._doc, movies: response[1] })
+        })
         .catch(err => res.status(500).json({ err: err.message }))
+})
+
+router.get("/:id/vehicles", (req, res) => {
+    let { id } = req.params
+    const charById = Character.findById(id)
+        .select('name gender skin_color hair_color height eye_color birth_year')
+    const vehicles = Vehicle.where({ pilots: id })
+        .select('vehicle_class')
+    Promise.all([charById, vehicles])
+        .then(response => {
+            res.status(200).json({ ...response[0]._doc, vehicles: response[1] })
+        })
+        .catch(error => console.log(error))
 })
 
 module.exports = router;
